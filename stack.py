b982f338d559416c936f4bb00c64bf023c9b9470
Dubhad <dubhad.connect@gmail.com>
2018-05-14 15:25:48 +0300

    Stack added

---
+++ stack.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, data):
+        node = Node(data)
+        node.next = self.head
+        self.head = node
+
+    def pop(self):
+        if self.head:
+            popped = self.head.data
+            self.head = self.head.next
+            return popped
+
+
+if __name__ == '__main__':
+    st = Stack()
+    st.push(4)
+    st.push(7)
+    st.push(11)
+    st.push(51)
+    st.push(2)
+    print(st.pop())
+    print(st.pop())
