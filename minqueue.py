aadcb213f27c6c420ad1c9569eea8faff32300be
Dubhad <dubhad.connect@gmail.com>
2018-05-14 17:57:46 +0300

    Added min queue with 2 stacks

--- minqueue.py
+++ minqueue.py
@@ -8,6 +8,9 @@ class MinStack:
         self.stack = Stack()
         self.min_stack = Stack()
 
+    def is_empty(self):
+        return self.stack.is_empty()
+
     def push(self, data):
         if self.stack.is_empty():
             self.stack.push(data)
@@ -30,6 +33,28 @@ class MinStack:
             return self.min_stack.peek()
 
 
+class MinQueue:
+    def __init__(self):
+        self.stack_1 = MinStack()
+        self.stack_2 = MinStack()
+
+    def enqueue(self, data):
+        self.stack_1.push(data)
+
+    def dequeue(self):
+        if self.stack_2.is_empty():
+            while not self.stack_1.is_empty():
+                self.stack_2.push(self.stack_1.pop())
+        return self.stack_2.pop()
+
+    def get_minimum(self):
+        if self.stack_1.is_empty():
+            return self.stack_2.get_minimum()
+        if self.stack_2.is_empty():
+            return self.stack_1.get_minimum()
+        return min(self.stack_1.get_minimum(), self.stack_2.get_minimum())
+
+
 if __name__ == '__main__':
     st = MinStack()
     st.push(5)
@@ -38,3 +63,12 @@ if __name__ == '__main__':
     st.push(3)
     st.push(8)
     print(st.get_minimum())
+
+    q = MinQueue()
+    q.enqueue(1)
+    q.enqueue(7)
+    q.enqueue(3)
+    q.enqueue(8)
+    q.enqueue(2)
+    q.enqueue(77)
+    print(q.get_minimum())
